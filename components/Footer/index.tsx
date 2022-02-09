2f2736dc240161630915585255e999a7d7899f75
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2022-02-09 14:19:06 +0100

    chore: footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-8 text-center text-base text-gray-400">
-          &copy; 2022 Torden Torg. All rights reserved.
+          &copy; 2022 Torden Torg AS
         </p>
       </div>
     </footer>
