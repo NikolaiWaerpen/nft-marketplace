5b38eafd62ef30264e1bbf37dcdbfc7c69f52850
Nikolai Waerpen <nikolaiwaerpen@gmail.com>
2022-04-12 11:50:07 +0200

    chore: further cleanup

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    OPENSEA_API_KEY: process.env.OPENSEA_API_KEY,
+  },
+};
--- utils/authorized-fetch.tsx
+++ utils/authorized-fetch.tsx
@@ -1,7 +1,11 @@
 export default function authorizedFetch(url: string) {
+  const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY;
+
+  if (!OPENSEA_API_KEY) throw new Error("missing opensea API key");
+
   return fetch(url, {
     headers: {
-      "X-API-KEY": "e7c75f6bcbca43d8b72d2ae91ace633b",
+      "X-API-KEY": OPENSEA_API_KEY,
     },
   });
 }
